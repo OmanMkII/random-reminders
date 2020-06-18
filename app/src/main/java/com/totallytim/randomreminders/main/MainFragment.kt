e683ca13d71f3de810c255150572097648b0a6b8
Eamon <the.oman.2@gmail.com>
2020-06-18 17:05:01 +1000

    Added a main fragment for clarity; overhauled frgments and deleted a few duplicates; reformatted some sections for consistent formatting

--- app/src/main/java/com/totallytim/randomreminders/main/MainFragment.kt
+++ app/src/main/java/com/totallytim/randomreminders/main/MainFragment.kt
@@ -1,22 +1,20 @@
-package com.totallytim.randomreminders.newreminder
+package com.totallytim.randomreminders.main
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.totallytim.randomreminders.R
-import com.totallytim.randomreminders.databinding.FragmentNewReminderBinding
+import com.totallytim.randomreminders.databinding.FragmentMainBinding
+import com.totallytim.randomreminders.newreminder.NewReminderViewModel
 
-class NewReminderFragment : Fragment() {
+class MainFragment : Fragment() {
 
 //    private lateinit var binding: NewReminderFragmentBinding
-    private lateinit var viewModel: NewReminderViewModel
+//    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
                 inflater: LayoutInflater,
@@ -24,10 +22,10 @@ class NewReminderFragment : Fragment() {
                 savedInstanceState: Bundle?
             ): View? {
 
-        viewModel = ViewModelProvider(this).get(NewReminderViewModel::class.java)
+//        viewModel = ViewModelProvider(this).get(NewReminderViewModel::class.java)
 //        binding = DataBindingUtil.inflate<FragmentGameBinding>(this, R.layout.fragment_settings, container, false)
 
-        val binding = DataBindingUtil.inflate<FragmentNewReminderBinding>(
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(
             inflater, R.layout.fragment_new_reminder, container, false)
 
 //        val view: View = inflater.inflate(R.layout.fragment_settings, container, false)
--- app/src/main/java/com/totallytim/randomreminders/settings/SettingsFragment.kt
+++ app/src/main/java/com/totallytim/randomreminders/settings/SettingsFragment.kt
@@ -1,5 +1,31 @@
 package com.totallytim.randomreminders.settings
 
-class SettingsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.totallytim.randomreminders.R
+import com.totallytim.randomreminders.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var viewModel: SettingsViewModel
+
+    override fun onCreateView(
+                inflater: LayoutInflater,
+                container: ViewGroup?,
+                savedInstanceState: Bundle?
+            ): View? {
+
+        viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
+
+        val binding = DataBindingUtil.inflate<FragmentSettingsBinding>(
+            inflater, R.layout.fragment_settings, container, false)
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
 }
