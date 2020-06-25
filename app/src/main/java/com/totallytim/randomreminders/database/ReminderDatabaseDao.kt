c3ac8b066b245eb98e83bbfb9feeb67177c12238
Eamon <the.oman.2@gmail.com>
2020-06-25 12:41:33 +1000

    Added queries for Settings objects, minor tweaks to allow nullable entries on some queries

--- app/src/main/java/com/totallytim/randomreminders/database/ReminderDatabaseDao.kt
+++ app/src/main/java/com/totallytim/randomreminders/database/ReminderDatabaseDao.kt
@@ -13,8 +13,37 @@ import androidx.room.Update
 interface ReminderDatabaseDao {
 
     // TODO: ensure I can grab all the proper fields
-    // TODO: make queries to grab all settings, certain setting
     // TODO: make method to set all setting fields to a default value
+
+
+    /** Settings Queries */
+
+    /**
+     * Inserts a new Setting to the data table
+     */
+    @Insert
+    fun insertSetting(setting: Setting)
+
+    /**
+     * Updates an existing setting
+     */
+    @Update
+    fun updateSetting(setting: Setting)
+
+    /**
+     * Selects all setting from the table
+     */
+    @Query("SELECT * FROM settings_table")
+    fun getAllSettings(): LiveData<List<Setting>>
+
+    /**
+     * Selects a specific setting from the data table
+     */
+    @Query("SELECT * FROM settings_table WHERE field_name = :key")
+    fun getOneSetting(key: String): LiveData<Setting?>
+
+
+    /** Day Queries */
 
     /**
      * Inserts a new Reminder object.
@@ -27,6 +56,15 @@ interface ReminderDatabaseDao {
      */
     @Update
     fun updateDay(day: Day)
+
+    /**
+     * Gets all Days from the database
+     */
+    @Query("SELECT * FROM daily_schedule")
+    fun getAllDays(): LiveData<List<Day>>
+
+
+    /** Reminder Queries */
 
     /**
      * Inserts a new Reminder object.
@@ -50,23 +88,23 @@ interface ReminderDatabaseDao {
      * Clears all existing entries from the database.
      */
     @Query("DELETE FROM reminders_table")
-    fun clearAll()
+    fun clearAllReminders()
 
     /**
      * Selects an existing entry given the name.
      */
     @Query("SELECT * FROM reminders_table WHERE reminder_name = :key")
-    fun get(key: String): LiveData<Reminder>
+    fun getReminder(key: String): LiveData<Reminder?>
 
     /**
      * Selects all existing entries, ordered by the next occurrence.
      */
     @Query("SELECT * FROM reminders_table ORDER BY next_occurrence DESC")
-    fun getAll(): LiveData<List<Reminder>>
+    fun getAllReminder(): LiveData<List<Reminder>>
 
     /**
      * Selects all existing extries and orders by next occurring, with a variable limit on volume.
      */
     @Query("SELECT * FROM reminders_table ORDER BY next_occurrence DESC LIMIT :num")
-    fun getNext(num: Int): LiveData<List<Reminder>?>
+    fun getNextReminder(num: Int): LiveData<List<Reminder>?>
 }
